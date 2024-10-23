eb9c94e769df0bfe490ee245b04056d82d208442
guillemlloret <guillem.lloret.macia@estudiantat.upc.edu>
2024-10-23 10:06:23 +0200

    animaciopocha

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -6,37 +6,19 @@ using UnityEngine.InputSystem;
 public class CharacterMovement : MonoBehaviour
 {
     Vector2 movementInput;
-    Animator _animator;
-    Rigidbody rb;
-    public float speed = 2;
+
+    public Vector2 Move => movementInput;
+
+   
     
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        _animator = rb.GetComponent<Animator>();
-        _animator.SetBool("Walking", false);
-        
-        
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        DoMove();
-       
-
-    }
+   
     private void OnMove(InputValue value)
     {
         Debug.Log("hola");
         movementInput = value.Get<Vector2>();
-        _animator.SetBool("Walking", true);
+       
+   
     }
-    private void DoMove ()
-    {
-        rb.transform.Translate(movementInput * speed * Time.deltaTime);
-        
-    }
+   
 }
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController _characterController;
+    CharacterMovement _input;
+    public float Speed = 1;
+
+    private Vector3 _lastVelocity;
+
+    Animator _animator;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _input = GetComponent<CharacterMovement>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        var horizontalSpeed = _lastVelocity;
+     
+        horizontalSpeed.y = 0;
+
+        Debug.Log(horizontalSpeed.magnitude);
+
+        _animator.SetFloat("speed", horizontalSpeed.magnitude*10);
+    }
+
+    
+
+    private void Move()
+    {
+        Vector3 direction = new Vector3(_input.Move.x, 0, _input.Move.y);
+        //_characterController.SimpleMove(direction * Speed);
+
+        Vector3 velocity = direction * Speed;
+        velocity.x = direction.x * Speed;
+        velocity.y = _lastVelocity.y;
+        velocity.z = direction.z * Speed;
+
+
+
+        //velocity.y = GetGravity();
+
+       
+
+        _characterController.Move(velocity * Time.deltaTime);
+
+        //turn
+        if (direction.magnitude > 0)
+        {
+            Vector3 target = transform.position + direction;
+            transform.LookAt(target);
+        }
+        _lastVelocity = velocity;
+    }
+
+    private float GetGravity()
+    {
+        return _lastVelocity.y + Physics.gravity.y * Time.deltaTime;
+    }
+}
